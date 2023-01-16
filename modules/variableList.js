9ef433ae21e73dd7ec29fe0eff4054d7c82734e4
Raphael <okolonnaemeka2017@gmail.com>
2023-01-16 07:27:20 -0500

    Add variables for site

---
+++ modules/variableList.js
@@ -0,0 +1,11 @@
+const list = document.querySelector('#list');
+const addNew = document.querySelector('#add-new');
+const contact = document.querySelector('.contact');
+const bookContainer = document.querySelector('.booksList');
+const form = document.querySelector('.form');
+const bookTitle = document.querySelector('.title');
+const bookAuthor = document.querySelector('.author');
+const addBtn = document.querySelector('#addBtn');
+
+
+export {list,addNew,contact,bookContainer,form,bookTitle,bookAuthor,addBtn};
